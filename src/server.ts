430bc9f0912de2846ba402df73de0f922d2efb7b
Nikita Shevhcuk <shevchuk.nikita.gh@gmail.com>
2023-03-23 22:42:19 +0200

    Add .env.example

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import cors from "cors";
 import express, { type Application } from "express";
 import { connectToDatabase } from "./utils/db";
 import { setupRoutes } from "./routes/paths";
+import session from "express-session";
 
 const port = process.env.PORT;
+const sessionSecret = process.env.SESSION_SECRET;
+const databaseLink = process.env.DATABASE_CONNECTION_LINK;
 
 const validatePortFromEnv = (port: string | undefined): boolean =>
     port !== undefined && port.length > 0 && !Number.isNaN(port);
@@ -14,7 +17,9 @@ export class ExpressServer {
     private readonly port: number;
 
     constructor() {
-        void connectToDatabase(process.env.DATABASE_CONNECTION_LINK as string);
+        // TODO: add environment variables validation
+        if (databaseLink === undefined) throw new Error("Database connection link is undefined!");
+        void connectToDatabase(databaseLink);
         this.app = express();
         this.port = validatePortFromEnv(port) ? Number(port) : 5000;
         this.middleware();
@@ -24,6 +29,9 @@ export class ExpressServer {
     middleware(): void {
         this.app.use(bodyParser.json());
         this.app.use(cors());
+        // TODO: add environment variables validation
+        if (sessionSecret === undefined) throw new Error("Session secret is undefined!");
+        this.app.use(session({ secret: sessionSecret, resave: false, saveUninitialized: false }));
     }
 
     routes(): void {
