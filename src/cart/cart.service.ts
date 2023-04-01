fdf303ed4ebfa4f5c88e57bce47a1f2c7202eda6
Nikita Shevhcuk <shevchuk.nikita.gh@gmail.com>
2023-04-01 18:42:55 +0300

    Refactor services

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,6 +1,7 @@
 import type { DefaultResponse } from "../Types/Response";
 import type { NewCartItem } from "../models/methods/user";
 import { type ICart } from "../models/types/user";
+import { createErrorResult } from "../utils/result";
 import CartRepository from "./cart.repository";
 
 class CartService {
@@ -17,12 +18,9 @@ class CartService {
         userId: string
     ): Promise<DefaultResponse<ICart | undefined>> {
         const updatedCart = await CartRepository.addCartItem(newCartItem, userId);
-        if (updatedCart === null) {
-            return {
-                success: false,
-                message: `Product with ID ${String(newCartItem.productId)} doesn't exist`
-            };
-        }
+        const productId = newCartItem.productId;
+        if (updatedCart === null)
+            return createErrorResult(`Product with ID ${productId} doesn't exist`);
         return {
             success: true,
             data: updatedCart
@@ -34,12 +32,9 @@ class CartService {
         userId: string
     ): Promise<DefaultResponse<ICart | undefined>> {
         const updatedCart = await CartRepository.removeOne(cartItemToRemove, userId);
-        if (updatedCart === null) {
-            return {
-                success: false,
-                message: `Product with id "${String(cartItemToRemove.productId)}" is not found!`
-            };
-        }
+        const productId = cartItemToRemove.productId;
+        if (updatedCart === null)
+            return createErrorResult(`Cart item with id "${productId}" is not found!`);
         return {
             success: true,
             data: updatedCart
@@ -48,12 +43,8 @@ class CartService {
 
     async removeAll(userId: string): Promise<DefaultResponse<ICart | null | undefined>> {
         const updatedCart = await CartRepository.removeAll(userId);
-        if (updatedCart === null) {
-            return {
-                success: false,
-                message: `Can't clear cart for user with ID: "${userId}"!`
-            };
-        }
+        if (updatedCart === null)
+            return createErrorResult(`Can't clear cart for user with ID: "${userId}"!`);
         return {
             success: true,
             data: updatedCart
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,47 +1,31 @@
 import type { DefaultResponse } from "../Types/Response";
 import { type IOrder } from "../models/types/order";
+import { createErrorResult } from "../utils/result";
 import OrderRepository from "./order.repository";
 
 class OrderService {
     async get(userId: string): Promise<DefaultResponse<IOrder[] | undefined>> {
         const orders = await OrderRepository.get(userId);
-        if (orders === null) {
-            return {
-                success: false,
-                message: "Orders for this user are not available"
-            };
-        }
+        if (orders === null) return createErrorResult("Orders for this user are not available");
         return {
             success: true,
             data: orders
         };
     }
 
-    async create(userId: string): Promise<DefaultResponse<IOrder | null>> {
+    async create(userId: string): Promise<DefaultResponse<IOrder | undefined>> {
         const newOrder = await OrderRepository.create(userId);
-        if (newOrder === null) {
-            return {
-                success: false,
-                message: "Can not create order since cart is empty."
-            };
-        }
+        if (newOrder === null)
+            return createErrorResult("Can not create order since cart is empty.");
         return {
             success: true,
             data: newOrder
         };
     }
 
-    async removeOne(
-        orderId: string,
-        userId: string
-    ): Promise<DefaultResponse<IOrder | null | undefined>> {
+    async removeOne(orderId: string, userId: string): Promise<DefaultResponse<IOrder | undefined>> {
         const removedOrder = await OrderRepository.removeOne(orderId, userId);
-        if (removedOrder === null) {
-            return {
-                success: false,
-                message: "Order ID is not valid!"
-            };
-        }
+        if (removedOrder === null) return createErrorResult("Order ID is not valid!");
         return {
             success: true,
             data: removedOrder
